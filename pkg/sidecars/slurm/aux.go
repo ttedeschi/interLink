8c37c81a8f387772293d7c6a0025bed832f50a48
Diego Ciangottini <diego.ciangottini@pg.infn.it>
2023-10-31 00:02:54 +0100

    stderr redirect

--- pkg/sidecars/slurm/aux.go
+++ pkg/sidecars/slurm/aux.go
@@ -233,7 +233,7 @@ func produce_slurm_script(podUID string, metadata metav1.ObjectMeta, commands []
 	stringToBeWritten += sbatch_macros
 
 	for _, singularityCommand := range commands {
-		stringToBeWritten += "\n" + strings.Join(singularityCommand.command[:], " ") + " >> " + commonIL.InterLinkConfigInst.DataRootFolder + podUID + "_" + singularityCommand.containerName + ".out 2>&1; echo $? > " + commonIL.InterLinkConfigInst.DataRootFolder + podUID + "_" + singularityCommand.containerName + ".status &"
+		stringToBeWritten += "\n" + strings.Join(singularityCommand.command[:], " ") + " &> " + commonIL.InterLinkConfigInst.DataRootFolder + podUID + "_" + singularityCommand.containerName + ".out; echo $? > " + commonIL.InterLinkConfigInst.DataRootFolder + podUID + "_" + singularityCommand.containerName + ".status &"
 	}
 
 	stringToBeWritten += "\n" + postfix
