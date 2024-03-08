60fad9c32712863a764e2ca2328c3fbd6d13db11
Diego Ciangottini <diego.ciangottini@pg.infn.it>
2024-03-08 21:34:14 +0100

    redocly openAPI in docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,6 +1,7 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import type * as Redocusaurus from 'redocusaurus';
 
 const config: Config = {
   title: 'interLink',
@@ -46,6 +47,27 @@ const config: Config = {
         },
       } satisfies Preset.Options,
     ],
+    [
+     'redocusaurus',
+      {
+        // Plugin Options for loading OpenAPI files
+        specs: [
+          // Pass it a path to a local OpenAPI YAML file
+          {
+            // Redocusaurus will automatically bundle your spec into a single file during the build
+            id: 'using-single-yaml',
+            spec: 'openapi/openapi.json',
+            route: '/openapi/',
+          },
+        ],
+        // Theme Options for modifying how redoc renders them
+        theme: {
+          // Change with your site colors
+          primaryColor: '#1890ff',
+        },
+      },
+    ], 
+
   ],
 
   themeConfig: {
